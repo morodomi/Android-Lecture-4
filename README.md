417b81b58ab51a3273ac965c962668debddf8820
Masahiro Morodomi <shonan.shachu@gmail.com>
2012-07-28 19:44:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+##Author
+Masahiro Morodomi &lt;morodomi at gmail.com&gt;
+
+##Description
+
+Android Lecture 4 for Developers.
+
+In this Lecture, you will learn what is intent and how to use it.
+
+##Requirements
+ * Eclipse IDE for Java Developers
+ * Android SDK
+ * Android 2.1 or higher
+
+##Questions
+ * Add an map intent action to open tokyo station.
+ * Search "Kiradeco Picture" on Google Play with one button.
+ * Pick up an image from Gallery and show on the view. (This may take more than 30 min.)
+ 
+## References (Japanese)
+http://www.saturn.dti.ne.jp/npaka/android/intent/index.html
---
+++ src/net/morodomi/lecture4/IntentActivity.java
@@ -0,0 +1,60 @@
+package net.morodomi.lecture4;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+/**
+ * Activity for Lecture 4
+ * In this lecture, you will know abstract of Intent
+ * @author Masahiro Morodomi <morodomi at gmail.com>
+ */
+public class IntentActivity extends Activity implements OnClickListener {
+	// URL schemes
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		// set OnClickListeners
+		findViewById(R.id.mail).setOnClickListener(this);
+		findViewById(R.id.sms).setOnClickListener(this);
+		findViewById(R.id.tel).setOnClickListener(this);
+		findViewById(R.id.http).setOnClickListener(this);
+		findViewById(R.id.https).setOnClickListener(this);
+	}
+
+	/** Called when views are clicked */
+	@Override
+	public void onClick(View v) {
+		// start activity
+		Intent intent = null;
+		switch (v.getId()) {
+		// sms
+		case R.id.sms:
+			intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:09011112222"));
+			break;
+		// telephone
+		case R.id.tel:
+			intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:09011112222"));
+			break;
+		// browser
+		case R.id.http:
+			intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.yahoo.co.jp"));
+			break;
+		// browser(https)
+		case R.id.https:
+			intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.co.jp"));
+			break;
+		default:
+			break;
+		}
+		if(intent != null) {
+			startActivity(intent);
+		}
+	}
+}
